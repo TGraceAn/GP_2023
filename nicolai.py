87c9da5341c5c13f60476ed17b52803c75484d03
Binh Phan <ptbinh2003@gmail.com>
2024-01-11 18:14:02 +0700

    depth map with EMA

--- nicolai.py
+++ nicolai.py
@@ -1,7 +1,7 @@
 '''
 - train model with difference dataset --> robust
-- open webcam with openCV, combine with pytorch, load in the models and do depth estimation 
-- on live webcam feed 
+- open webcam with openCV, combine with pytorch, load in the models and do depth estimation
+- on live webcam feed
 '''
 
 import cv2
@@ -9,13 +9,11 @@ import torch
 import time
 import numpy as np
 
-
-
 # Load a MiDas model for depth estimation
-#model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+# model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
 # hybrid: mobile devices
-#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
-# small: low_budget computer --> the media smart model 
+# model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+# small: low_budget computer --> the media smart model
 
 model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
 
@@ -27,79 +25,18 @@ midas.to(device)
 midas.eval()
 
 # Load transforms to resize and normalize the image
-# load img --> numeric --> resize, normalize the images --> same input format 
+# load img --> numeric --> resize, normalize the images --> same input format
 midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 
 if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
-    # based on transformers, have different kind of input format 
+    # based on transformers, have different kind of input format
     transform = midas_transforms.dpt_transform
 else:
     transform = midas_transforms.small_transform
 
 # print(transform)
 
-
-# # Open up the video capture from a webcam
-# cap = cv2.VideoCapture(0)
-
-# while cap.isOpened():
-
-#     success, img = cap.read()
-    
-#     # time to transformer and input -> output 
-#     start = time.time()
-
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-#     # Apply input transforms, put it onto the device 
-#     input_batch = transform(img).to(device)
-
-#     # Prediction and resize to original resolution\
-#     # announce touch that we are doing inference but not training 
-#     with torch.no_grad():
-#         prediction = midas(input_batch)
-
-#         # model's output --> not normalized, return original shape ... --> openCV image  
-#         prediction = torch.nn.functional.interpolate(
-#             prediction.unsqueeze(1),
-#             size=img.shape[:2],
-#             mode="bicubic",
-#             align_corners=False,
-#         ).squeeze()
-
-#     # gpu --> cpu format
-#     depth_map = prediction.cpu().numpy()
-
-#     # normalize depth map: [0,1] 
-#     depth_map = cv2.normalize(depth_map, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
-
-
-#     end = time.time()
-#     totalTime = end - start
-
-#     # calculate # fps 
-#     fps = 1 / totalTime
-
-#     # return image color 
-#     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    
-#     # color depth math, unsign int data type  
-#     depth_map = (depth_map*255).astype(np.uint8)
-#     depth_map = cv2.applyColorMap(depth_map , cv2.COLORMAP_MAGMA)
-
-#     print(depth_map.shape)
-
-#     cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-#     cv2.imshow('Image', img)
-#     cv2.imshow('Depth Map', depth_map)
-
-#     if cv2.waitKey(5) & 0xFF == 27:
-#         break
-
-# cap.release()
-    
 depth_shape = (480,640)
-
 def depth_midas(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     input_batch = transform(img).to(device)
@@ -108,8 +45,8 @@ def depth_midas(img):
 
     with torch.no_grad():
         prediction = midas(input_batch)
-        
-        # model's output --> not normalized, return original shape ... --> openCV image  
+
+        # model's output --> not normalized, return original shape ... --> openCV image
         prediction = torch.nn.functional.interpolate(
             prediction.unsqueeze(1),
             size=depth_shape,
@@ -118,28 +55,4 @@ def depth_midas(img):
         ).squeeze()
 
         depth_map = prediction.cpu().numpy()
-        orginal_depth_map = depth_map.copy()
-
-        # Get center pixel value
-        center = depth_map[240, 320]
-        # Take depth_map center box
-        depth_center = depth_map[213:426, 160:320]
-        # calculate_depth = np.mean(depth_center)
-        max_depth = np.max(depth_center)
-        mean_depth = np.mean(depth_center)
-        median_depth = np.median(depth_center)
-
-        min_depth = np.min(depth_center)
-
-        depth_map = cv2.normalize(depth_map, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
-
-
-        print(f"Max: {max_depth}, Mean: {mean_depth}, Min: {min_depth}, Median: {median_depth}, Center: {center}")
-        
-        # print(calculate_depth)
-
-        # normalization
-        depth_map = (depth_map*255).astype(np.uint8)
-        depth_map = cv2.applyColorMap(depth_map , cv2.COLORMAP_MAGMA)
-        
-        return depth_map, orginal_depth_map
+        return depth_map
